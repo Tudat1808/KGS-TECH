6cdfc703e36be30ed435a4f5e11cff08aad90ec2
Tudat1808 <tieutudat1808.hyggelig@gmail.com>
2025-01-03 08:10:05 +0700

    ページをリンクします。

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,10 +37,10 @@ const Header = ({ home, company, business, blog }) => {
       {/* Menu chính */}
       <div className={`flex items-center gap-8 w-full justify-end ${menuOpen ? 'block' : 'hidden'} md:flex`}>
         <ul className="flex gap-8 md:flex-row flex-col justify-end">
-          <li className="hover:text-gray-400"><a href="/home" className="hover:text-blue-400">Home</a></li>
-          <li className="hover:text-gray-400"><a href="/company" className="hover:text-blue-400">Company</a></li>
-          <li className="hover:text-gray-400"><a href="/bussiness" className="hover:text-blue-400">Bussiness</a></li>
-          <li className="hover:text-gray-400"><a href="/blog" className="hover:text-blue-400">Blog</a></li>
+          <li className="hover:text-gray-400"> <Link to="/home">home</Link> </li>
+          <li className="hover:text-gray-400"><Link to="/company">company</Link></li>
+          <li className="hover:text-gray-400"><Link to="/bussiness">bussiness</Link></li>
+          <li className="hover:text-gray-400"><Link to="/blog">blog</Link></li>
         </ul>
 
         {/* Dropdown Select for Language */}
